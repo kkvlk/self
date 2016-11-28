7e46867ff312d4e08d25bddd7cfebc8f37a1c988
Kris Kovalik <hi@kkvlk.me>
2016-11-28 11:07:56 +0100

    Intense styling.
    - Added h4x0r theme.
    - Neat animations.
    - Pretty complete styling of the timeline.
    - Scripting to control animations and possibly music.

--- public/scripts/animations.js
+++ public/scripts/animations.js
@@ -1,11 +1,30 @@
 $(function() {
+  var hasLocalStorage = typeof(Storage) !== "undefined";
+
   $("#resume position h3").each(function(_, el) {
     var $el = $(el);
     $el.attr("data-text", $el.text());
   });
 
-  $("a.toggle").click(function(el) {
-    var $el = $(el.target);
-    $("html").toggleClass("no-" + $el.data("toggle"));
+  $("a.toggle").click(function(event) {
+    var $el = $(event.target);
+    var $html = $("html");
+    var feature = $el.data("toggle");
+
+    $html.toggleClass("no-" + feature);
+
+    if (hasLocalStorage) {
+      localStorage.htmlClass = $html.attr("class");
+    }
+
+    event.preventDefault();
+    return false;
   });
-})
+
+  if (hasLocalStorage) {
+    var htmlClass = localStorage.htmlClass;
+    if (htmlClass !== undefined) {
+      $("html").addClass(htmlClass);
+    }
+  }
+});
